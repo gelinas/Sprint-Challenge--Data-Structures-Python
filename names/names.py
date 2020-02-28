9a996ada6ccaf15f3b3a6dc399d3a15dc7d801f5
gelinas <jlgelinas@gmail.com>
2020-02-28 12:41:37 -0500

    feat: find duplicates with binary search tree

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -13,10 +14,28 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# PLAN: 
+# 1) build binary search tree on names_1
+# 2) traverse binary serach tree with name 2 for duplicates
+
+# build binary search tree on names_1
+names_tree = BinarySearchTree(names_1[0])
+for i in range(1, len(names_1) - 1):
+    names_tree.insert(names_1[i])
+
+# traverse binary serach tree with names_2
+for name in names_2:
+    if names_tree.contains(name):
+        duplicates.append(name)
+
+# names_tree.in_order_print(names_tree)
+
+# 6 second runtime code
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
