89423cb2f5bc29d952624e4be337451d3fd90c14
gelinas <jlgelinas@gmail.com>
2020-02-28 13:36:12 -0500

    feat: stretch names problem runtime

---
+++ names/names_stretch.py
@@ -0,0 +1,37 @@
+import time
+from binary_search_tree import BinarySearchTree
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = []  # Return the list of duplicates in this data structure
+
+# ---------- Stretch Goal -----------
+# Python has built-in tools that allow for a very efficient approach to this problem
+# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# structures, but you may not import any additional libraries that you did not write yourself.
+
+names_dict = {}
+for name in names_1:
+    names_dict.update({name: 0})
+for name in names_2:
+    in_dict = names_dict.get(name, "")
+    if in_dict != "":
+        duplicates.append(name)
+
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+# ---------- Stretch Goal -----------
+# Python has built-in tools that allow for a very efficient approach to this problem
+# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# structures, but you may not import any additional libraries that you did not write yourself.
